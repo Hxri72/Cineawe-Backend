1706b6ab449433480f66da9f4a0e00f27bd9c037
Hariprasad K H <hariprasad72172@gmail.com>
2023-04-06 19:50:14 +0530

    bugs solved

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.json())
 app.use(
     cors({
       // origin: ["http://localhost:3000"],
-    origin: ["https://www.cineawe.online"],
+        origin: ["*"],
       methods: ["GET", "POST", "PUT", "DELETE","HEAD", "OPTIONS"],
       credentials: true,  
       exposedHeaders: ["Content-Length", "X-Foo", "X-Bar"],
