ff8bdab248df830ce10a93679cce49111a654f60
Hariprasad K H <hariprasad72172@gmail.com>
2023-04-07 00:55:32 +0530

    serverSide modified

--- server.js
+++ server.js
@@ -5,20 +5,22 @@ require('dotenv').config()
 
 app.use(express.json())
 
-app.use(function (req, res, next) {
+app.use(cors());
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
+// app.use(function (req, res, next) {
+
+//     // Website you wish to allow to connect
+//     res.setHeader('Access-Control-Allow-Origin', 'https://cineawe.online');
   
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//     // Request methods you wish to allow
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    // Pass to next layer of middleware
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
+//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//     // Pass to next layer of middleware
+//     res.setHeader('Access-Control-Allow-Credentials', 'true');
   
-    next();
-});
+//     next();
+// });
 
 //database connection
 require("./config/dbConfig")
